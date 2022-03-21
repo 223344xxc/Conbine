e0bf61fea4fac4e2cc7f9bc8d2b614a6a6b2dc37
MuYaHong2 <223344xxc@gmail.com>
2022-03-21 16:27:55 +0900

    MapManager
    기본 맵 타일 생성, MapManager 생성 후 맵 생성 로직 구현

---
+++ Assets/Script/IndexVector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct IndexVector
+{
+    public int x;
+    public int y;
+
+    public void CastIndexVector(Vector2 v)
+    {
+        x = (int)v.x;
+        y = (int)v.y;
+    }
+}
---
+++ Assets/Script/MapMgr.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMgr : MonoBehaviour
+{
+    [SerializeField] private Vector2 size; //임시 변수
+
+    [SerializeField] private float sellSize;
+    [SerializeField] private MapSell[][] map;
+    [SerializeField] private GameObject sellPrefab;
+    private GameObject mapParent;
+
+    private IndexVector mapSize;
+
+    private void Awake()
+    {
+        InitMapMgr();
+    }
+
+    private void InitMapMgr()
+    {
+        mapParent = GameObject.Find("Map");
+
+        mapSize.CastIndexVector(size);
+
+        Vector2 offset;
+        offset.x = (size.x / 2) * sellSize - (sellSize * 0.5f);
+        offset.y = (size.y / 2) * sellSize - (sellSize * 0.5f);
+
+
+        map = new MapSell[mapSize.y][];
+        for(int y = 0; y < mapSize.y; y++)
+        {
+            map[y] = new MapSell[mapSize.x];
+            for(int x = 0; x < mapSize.x; x++)
+            {
+                map[y][x] = Instantiate(sellPrefab, mapParent.transform).GetComponent<MapSell>();
+                map[y][x].transform.position = new Vector2(x * sellSize - offset.x, y * - sellSize + offset.y);
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
