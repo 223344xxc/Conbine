92349af5ce03ba842825d3a795a497774e2cad32
이진서 <223344xxc@gmail.com>
2022-05-08 22:29:57 +0900

    [UPDATE] 리소스 로드 함수 래핑
    리소스 로드 함수 래핑
    
    스프라이트 로드 함수 추가

--- Assets/Script/Manager/ResourceManager.cs
+++ Assets/Script/Manager/ResourceManager.cs
@@ -1,31 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MapSellTypeOptions;
 
 public static class ResourceManager
 {
     /// <summary>
     /// 리소스 파일 이름 입니다.
     /// </summary>
-    public static class ResourceName
+    public struct ResourceName
     {
+        #region 맵 프리펩 이름 정보
         public static string mapSellPrefab = "DefaultSell";
         public static string mapEditorSellPrefab = "MapEditorSell";
         public static string squarePrefab = "Square";
-
-        public static string lockOpenEffect = "LockOpen_Effect_Prefab";
+        public static string lockOpenEffectPrefab = "LockOpen_Effect_Prefab";
+        #endregion
     }
 
+    /// <summary>
+    /// Resources 클래스의 Load 함수를 래핑한 함수입니다.
+    /// 입력받은 경로에 있는 리소스를 반환합니다.
+    /// </summary>
+    /// <typeparam name="T"> 제네릭 타입은 Object 클래스의 자식이여야 합니다. </typeparam>
+    /// <param name="path"> 파일 이름을 포함한 경로입니다. </param>
+    /// <returns></returns>
+    public static T LoadResource<T>(string path) where T : Object
+    {
+        return Resources.Load<T>(path);
+    }
+
+    /// <summary>
+    /// 입력받은 경로의 스프라이트를 읽어 반환합니다.
+    /// </summary>
+    /// <param name="path"> 파일 경로 </param>
+    public static Sprite LoadSprite(string path)
+    {
+        Sprite spr = LoadResource<Sprite>(path);
+        if (spr)
+            return spr;
+        else
+            return null;
+    }
 
     /// <summary>
     /// 입력받은 경로의 프리펩을 읽어 반환합니다.
     /// </summary>
-    /// <param name="path">
-    /// 파일 경로
-    /// </param>
+    /// <param name="path"> 파일 경로 </param>
     public static GameObject LoadPrefab(string path)
     {
-        GameObject obj = Resources.Load<GameObject>(path);
+        GameObject obj = LoadResource<GameObject>(path);
         if (obj)
             return obj;
         else
@@ -39,7 +63,9 @@ public static class ResourceManager
     {
         return LoadPrefab(
             FilePathManager.ConnectPath(
-            FilePathManager.FileName.prefab, ResourceName.mapSellPrefab));
+            FilePathManager.FileName.prefab, 
+            ResourceName.mapSellPrefab)
+            );
     }
 
     /// <summary>
@@ -49,7 +75,9 @@ public static class ResourceManager
     {
         return LoadPrefab(
             FilePathManager.ConnectPath(
-            FilePathManager.FileName.prefab, ResourceName.mapEditorSellPrefab));
+            FilePathManager.FileName.prefab, 
+            ResourceName.mapEditorSellPrefab)
+            );
     }
 
     /// <summary>
@@ -58,7 +86,22 @@ public static class ResourceManager
     public static GameObject GetLockOpenEffect()
     {
         return LoadPrefab(FilePathManager.ConnectPath(
-            FilePathManager.FileName.prefab, FilePathManager.FileName.effect, ResourceName.lockOpenEffect));
+            FilePathManager.FileName.prefab, 
+            FilePathManager.FileName.effect, 
+            ResourceName.lockOpenEffectPrefab)
+            );
 
+    }
+
+    /// <summary>
+    /// 맵 타입에 맞는 스프라이트를 반환합니다.
+    /// </summary>
+    public static Sprite GetMapSellSprite(MapSellType mapSellType)
+    {
+        return LoadSprite(FilePathManager.ConnectPath(
+            FilePathManager.FileName.sprite, 
+            FilePathManager.FileName.mapSell, 
+            MapSellType.GetMapSellSpriteName(mapSellType.sellTypeCode)
+            ));
     }
 }
