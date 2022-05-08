dd20b6f7f8477f2ccf6ec4b04b0b8d47870007a0
이진서 <223344xxc@gmail.com>
2022-05-08 23:35:22 +0900

    [UPDATE] 사용자 커서 입력 수정
    ObjectClicker 컴포넌트 추가
    UserRaycastManager 추가

---
+++ Assets/Script/Game/ObjectClicker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectClicker : MonoBehaviour
+{
+    private Action onClickDown;
+    private Action onClick;
+    private Action onClickUp;
+
+    public void BindOnClickDown(Action action)
+    {
+        onClickDown += action;
+    }
+
+    public void BindOnClick(Action action)
+    {
+        onClick += action;
+    }
+
+    public void BindOnClickUp(Action action)
+    {
+        onClickUp += action;
+    }
+
+    /// <summary>
+    /// 오브젝트가 클릭되면 호출됩니다.
+    /// </summary>
+    public void OnClickDown()
+    {
+        onClickDown?.Invoke();
+    }
+
+    /// <summary>
+    /// 오브젝트가 클릭중일때 호출됩니다.
+    /// </summary>
+    public void OnClick()
+    {
+        onClick?.Invoke();
+    }
+
+    /// <summary>
+    /// 오브젝트가 클릭해제되면 호출됩니다.
+    /// </summary>
+    public void OnClickUp()
+    {
+        onClickUp?.Invoke();
+    }
+}
---
+++ Assets/Script/Manager/UserRaycastManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserRaycastManager : MonoBehaviour
+{
+    public static UserRaycastManager instance;
+
+    private RaycastHit2D hitObject;
+
+    private void Awake()
+    {
+        InitUserRaycastManager();
+    }
+
+    private void InitUserRaycastManager()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        RaycastUpdate();
+    }
+
+    private void RaycastUpdate()
+    {
+        hitObject = Physics2D.Raycast(
+        Camera.main.ScreenToWorldPoint(Input.mousePosition),
+        Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    public RaycastHit2D GetHitObject()
+    {
+        return hitObject;
+    }
+}
