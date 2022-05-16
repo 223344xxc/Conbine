d167789afb5d5865e1761a82cf489ca3f0342c26
이진서 <74497675+223344xxc@users.noreply.github.com>
2022-05-16 14:07:56 +0900

    [UPDATE]

---
+++ Assets/Script/TestClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestClick : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        "TEST".Log();
+    }
+}
