9795a1de6b39bd2e0fca900585a1eebba26afac8
이진서 <74497675+223344xxc@users.noreply.github.com>
2022-05-17 12:47:11 +0900

    [ADD] UI 클릭 컴포넌트 추가

---
+++ Assets/Script/Component/UIClicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class UIClicker : MonoBehaviour, IPointerClickHandler
+{
+    private Action onClickEvent;
+
+    public void SetOnClickEvent(Action action){
+        onClickEvent += action;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClickEvent?.Invoke();
+    }
+}
